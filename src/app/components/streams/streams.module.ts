57a3addbf267d265c2c9c96df5541c26b9992993
Aml Fakhri <amlfakhry13@gmail.com>
2022-03-07 13:57:15 +0200

    add side nav.

--- src/app/components/streams/streams.module.ts
+++ src/app/components/streams/streams.module.ts
@@ -5,9 +5,12 @@ import { MaterialModule } from '../material/material.module';
 import { StreamsRoutingModule } from './streams-routing.module';
 import { StreamsComponent } from './streams/streams.component';
 import { NavbarComponent } from '../utils/navbar/navbar.component';
+import { SidenavComponent } from '../utils/sidenav/sidenav.component';
+import { TokensService } from 'src/app/services/tokens.service';
 
 @NgModule({
-  declarations: [StreamsComponent, NavbarComponent],
+  declarations: [StreamsComponent, NavbarComponent, SidenavComponent],
   imports: [CommonModule, StreamsRoutingModule, MaterialModule],
+  providers: [TokensService],
 })
 export class StreamsModule {}
